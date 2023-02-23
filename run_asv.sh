ace8248ab0a9ea51e3819fd1f451a8b60dfca781
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-02-23 14:02:03 +0000

    Use taskset

--- run_asv.sh
+++ run_asv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 # Last commit before FITS changes
-asv run -e 9409d02ff9e101d4215f83aa1f9a23d805230fd0^!
+taskset -c 0 asv run -e 9409d02ff9e101d4215f83aa1f9a23d805230fd0^!
 
 # All commits since
-asv run -e NEW
+taskset -c 0 asv run -e NEW
