0be30ac9e611ccd0cabd5948fc32419fbb945bae
Stuart Mumford <stuart@cadair.com>
2023-03-06 13:02:26 +0000

    A bunch of dkist stuff

--- benchmarks.py
+++ benchmarks.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+from functools import partial
+
+import numpy as np
+from astropy.io.fits._tiled_compression.utils import _n_tiles
 from astropy.io import fits
 import dask
 dask.config.set(scheduler='synchronous')  # overwrite default with single-threaded scheduler
+# dask.config.set(scheduler='threads')
 
 BENCHMARKS = {}
 
@@ -42,19 +47,48 @@ def access_data_aia(hdu):
     return hdu.data
 
 
-def load_dkist_dask_array():
+def load_dkist_dask_array(path):
     import dkist
-    ds = dkist.Dataset.from_directory("/storfa/dkist/globus/pid_1_118/AKNPB")
-    print(ds.data)
+    ds = dkist.Dataset.from_directory(path)
+    print(f"Using DKIST dataset ({ds.meta['inventory']['datasetId']}) with shape: {ds.data.shape}")
+    with fits.open(ds.files.basepath / ds.files.filenames[0], disable_image_compression=True) as hdul:
+        h = hdul[1].header
+        tile_shape = tuple(h[f"ZTILE{z}"] for z in range(1, h["ZNAXIS"] + 1))[::-1]
+        print(f"FITS tile shape is {tile_shape} (numpy order).")
+        print(f"File has {np.product(_n_tiles(hdul[1].data.shape, tile_shape))} tiles.")
     return ds.data,
 
 
-@benchmark(setup=load_dkist_dask_array)
+def load_AKNPB():
+    return load_dkist_dask_array("/storfa/dkist/globus/pid_1_118/AKNPB")
+
+
+@benchmark(setup=load_AKNPB)
 def dkist_access_single_frame(data):
     data[0, 0, 0].compute()
 
 
-@benchmark(setup=load_dkist_dask_array)
+@benchmark(setup=load_AKNPB)
+def dkist_access_mosaic(data):
+    data[0, :, 0, :].compute()
+
+
+@benchmark(setup=load_AKNPB)
 def dkist_access_single_chunk(data):
     arr = data[0, 0, 0, 0].compute()
     print(arr.shape)
+
+
+def load_simulated_dkist(dataset):
+    base = Path("/storfa/dkist/tiled_performance")
+    return load_dkist_dask_array(base / dataset)
+
+
+@benchmark(partial(load_simulated_dkist, "row_tiles"))
+def dkist_load_mosaic_row(data):
+    return data[0, :, 0, :].compute()
+
+
+@benchmark(partial(load_simulated_dkist, "square_tiles"))
+def dkist_load_mosaic_square(data):
+    return data[0, :, 0, :].compute()
